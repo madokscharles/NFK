06f7280eedff4aa1583c05b40914f6af13408461
madokscharles <charlesmadoks@gmail.com>
2023-02-01 16:44:40 +0100

    Corrections

--- hello/urls.py
+++ hello/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet"),
-    path("madoks", views.madoks, name="madoks"),
-    path("ben", views.ben, name="ben")
+    path('', views.index, name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,13 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from NFK!")
-
-def madoks(request):
-    return HttpResponse("Hello, Madoks!")
-
-def ben(request):
-    return HttpResponse("Hello, Ben!")
-
-def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    return HttpResponse("Hello world!")

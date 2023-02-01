77076a03147ee477acdff119ab57a9e6bb94a167
madokscharles <charlesmadoks@gmail.com>
2023-02-01 18:52:54 +0100

    HTML and CSS added

--- hello/urls.py
+++ hello/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:name>', views.greet, name='greet'),
+    path('madoks', views.madoks, name='madoks'),
+    path('ben', views.ben, name='ben')
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, "hello/index.html")
+
+def madoks(request):
+    return HttpResponse("Hello, Madoks!")
+
+def ben(request):
+    return HttpResponse("Hello, Ben!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })

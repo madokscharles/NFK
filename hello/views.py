4ca5a56521b34baa4bca130ad7532968af5c773b
madokscharles <charlesmadoks@gmail.com>
2023-02-01 12:16:00 +0100

    Creating new apps

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello from NFK!")
+
+def madoks(request):
+    return HttpResponse("Hello, Madoks!")
+
+def ben(request):
+    return HttpResponse("Hello, Ben!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name}!")
